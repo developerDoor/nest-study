38beec4a02f0a404e9f918c557322b378099c2f7
jhmoon <dothebestmoon1016@gmail.com>
2023-05-18 18:01:20 +0900

    googo

--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -2,12 +2,14 @@ import * as express from 'express';
 
 
 const app: express.Express = express()
-const port: number = 8000
+const port: number = 3000
+
+const data = [1, 2, 3, 4]
 
 app.get('/', (req: express.Request, res: express.Response) => {
-    res.send('Hello World!')
+    res.send({data})
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`server is on ${port}`)
 })
