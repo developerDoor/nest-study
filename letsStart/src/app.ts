79e941eadcf04d2c591b9c16f2eb0860645392a5
Moon_JangHyeok <dothebestmoon1016@gmail.com>
2023-05-21 15:52:02 +0900

    express 미들웨어

--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -4,10 +4,29 @@ import { Cat, CatType } from "./app.model";
 const app: express.Express = express();
 const port: number = 3000;
 
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log("this is logging middleware");
+  next();
+});
+
 app.get("/", (req: express.Request, res: express.Response) => {
   console.log(req);
   res.send({ cats: Cat });
 });
+
+app.use(
+  (
+    err: Error,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    console.log(req.rawHeaders[1]);
+    console.log("this is error middleware");
+    res.send({ error: "404 not found error" });
+  }
+);
 
 app.listen(port, () => {
   console.log(`server is on ${port}`);
