f1429c57f07d0f48e694a77c670f1853ab65a6bd
jhmoon <dothebestmoon1016@gmail.com>
2023-05-23 08:56:48 +0900

    route 분리, 모듈화

--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -1,33 +1,27 @@
 import * as express from "express";
-import { Cat, CatType } from "./app.model";
+import catsRouter from './cats/cats.route'
 
 const app: express.Express = express();
 const port: number = 3000;
 
+// 로깅 미들웨어
 app.use((req, res, next) => {
-  console.log(req.rawHeaders[1]);
-  console.log("this is logging middleware");
-  next();
-});
-
-app.get("/", (req: express.Request, res: express.Response) => {
-  console.log(req);
-  res.send({ cats: Cat });
-});
-
-app.use(
-  (
-    err: Error,
-    req: express.Request,
-    res: express.Response,
-    next: express.NextFunction
-  ) => {
     console.log(req.rawHeaders[1]);
+    console.log("this is logging middleware");
+    next();
+});
+
+// JSON 미들웨어
+app.use(express.json());
+
+app.use(catsRouter);
+
+// 404 미들웨어
+app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
     console.log("this is error middleware");
-    res.send({ error: "404 not found error" });
-  }
-);
+    res.send({error: "404 not found error"});
+});
 
 app.listen(port, () => {
-  console.log(`server is on ${port}`);
+    console.log(`server is on ${port}`);
 });
