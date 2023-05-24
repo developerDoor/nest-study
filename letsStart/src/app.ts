f75a6bcb9c7ccb6e252a53b451957eb35c024a2d
jhmoon <dothebestmoon1016@gmail.com>
2023-05-25 08:45:42 +0900

    express 싱글톤 패턴, 서비스 패턴 적용

--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -4,24 +4,54 @@ import catsRouter from './cats/cats.route'
 const app: express.Express = express();
 const port: number = 3000;
 
-// 로깅 미들웨어
-app.use((req, res, next) => {
-    console.log(req.rawHeaders[1]);
-    console.log("this is logging middleware");
-    next();
-});
+// 싱글톤 패턴
+// 최초의 한번 New 연산자를 통해서 인스턴스를 생성한다. 추후 객체에 접근을 할 때 메모리 낭비를 방지할 수 있다.
+// 또한 다른 클래스간의 데이터 공유가 쉽다.
 
-// JSON 미들웨어
-app.use(express.json());
+class Server {
+    public app: express.Application;
 
-app.use(catsRouter);
+    constructor() {
+        const app: express.Application = express();
+        this.app = app;
+    }
 
-// 404 미들웨어
-app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
-    console.log("this is error middleware");
-    res.send({error: "404 not found error"});
-});
+    private setRoute() {
+        this.app.use(catsRouter);
+    }
 
-app.listen(port, () => {
-    console.log(`server is on ${port}`);
-});
+    private setMiddleware() {
+        // 로깅 미들웨어
+        this.app.use((req, res, next) => {
+            console.log(req.rawHeaders[1]);
+            console.log("this is logging middleware");
+            next();
+        });
+
+        // JSON 미들웨어
+        this.app.use(express.json());
+
+        this.setRoute();
+
+        // 404 미들웨어
+        this.app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+            console.log("this is error middleware");
+            res.send({error: "404 not found error"});
+        });
+    }
+
+    public listen() {
+        this.setMiddleware();
+        this.app.listen(port, () => {
+            console.log(`server is on ${port}`);
+        });
+    }
+
+}
+
+function init() {
+    const server = new Server();
+    server.listen();
+}
+
+init();
