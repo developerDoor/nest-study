fb8eccb5502a9ba89f9eb7f6f12de49790ec651b
jhmoon <dothebestmoon1016@gmail.com>
2023-06-07 08:08:58 +0900

    Modules & 캡슐화

--- NestJS 실전 프로젝트/src/app.controller.ts
+++ NestJS 실전 프로젝트/src/app.controller.ts
@@ -6,8 +6,8 @@ import {Request} from "express";
 export class AppController {
   constructor(private readonly appService: AppService) {} // 이런 패턴을 DI(의존성 주입)이라한다. provider(공급자)가 컨트롤러에게 의존성을 주입해준다.
 
-  @Get('hello/:id/:name')
-  getHello(@Req() req: Request, @Body() Body, @Param() param): string {
+  @Get('')
+  getHello(): string {
     return this.appService.getHello();
   }
 }
--- NestJS 실전 프로젝트/src/app.module.ts
+++ NestJS 실전 프로젝트/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [CatsModule, UsersModule], // CatsModule, UsersModule 등등의 모듈에서 export 한 서비스들을 AppController나 AppService에서 사용할 수 있다.
   controllers: [AppController],
   providers: [AppService],
 })
