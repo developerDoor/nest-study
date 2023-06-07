b5171aa696a1ba364ef7e443901d6e8f556df217
jhmoon <dothebestmoon1016@gmail.com>
2023-06-08 08:29:02 +0900

    nest middleware

--- NestJS 실전 프로젝트/src/app.module.ts
+++ NestJS 실전 프로젝트/src/app.module.ts
@@ -1,12 +1,16 @@
-import { Module } from '@nestjs/common';
+import {MiddlewareConsumer, Module, NestModule} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
-import { UsersModule } from './users/users.module';
+import {LoggerMiddleware} from "./logger.middleware";
 
 @Module({
-  imports: [CatsModule, UsersModule], // CatsModule, UsersModule 등등의 모듈에서 export 한 서비스들을 AppController나 AppService에서 사용할 수 있다.
+  imports: [CatsModule], // CatsModule, UsersModule 등등의 모듈에서 export 한 서비스들을 AppController나 AppService에서 사용할 수 있다.
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(LoggerMiddleware).forRoutes('*');
+    }
+}
