56a043c40f6a823f68656aa0ff22490630d0ebf1
jhmoon <dothebestmoon1016@gmail.com>
2023-06-09 08:38:38 +0900

    Exception filter & pipes

--- NestJS 실전 프로젝트/src/app.controller.ts
+++ NestJS 실전 프로젝트/src/app.controller.ts
@@ -2,7 +2,7 @@ import {Body, Controller, Get, Param, Req} from '@nestjs/common';
 import { AppService } from './app.service';
 import {Request} from "express";
 
-@Controller('cats')
+@Controller('')
 export class AppController {
   constructor(private readonly appService: AppService) {} // 이런 패턴을 DI(의존성 주입)이라한다. provider(공급자)가 컨트롤러에게 의존성을 주입해준다.
 
